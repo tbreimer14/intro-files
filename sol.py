7ee0a071caa62b1cc366b77ada6acd5ecc8a3ea1
Thomas Breimer <150462258+tbreimer14@users.noreply.github.com>
2025-09-10 19:09:25 -0400

    Add files via upload

---
+++ sol.py
@@ -0,0 +1,19 @@
+def answer1():
+    # Change this expression so that it evaluates to True
+    return 5 > 7
+
+def answer2():
+    # Change this expression so that it evaluates to False
+    return 3.9 > 3.12
+
+def answer3():
+    # Change the operator so that it returns an "int"
+    return 8 / 5
+
+def answer4():
+    # Change the operator so that it returns a "float"
+    return 11 % 3
+
+def answer5():
+    # Change this variable to be a string
+    return 4.25
