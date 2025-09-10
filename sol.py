a1b651235a437ac91e8d4d71a3c8654b2dc71e0e
Thomas Breimer <150462258+tbreimer14@users.noreply.github.com>
2025-09-10 14:35:15 -0400

    Add files via upload

---
+++ sol.py
@@ -0,0 +1,14 @@
+# Change this expression so that it evaluates to True
+answer1 = 5 < 7
+
+# Change this expression so that it evaluates to False
+answer2 = 3.9 < 3.12
+
+# Change the operator in this expression so that it returns an "int"
+answer3 = 8 // 5
+
+# Change the operator in this expression so that it returns a "float"
+answer4 = 11 / 3
+
+# Change this variable to be a string
+answer5 = "4.25"
